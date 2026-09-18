7a8e389517d376f0a877e87cc2f2cae48bcc6830
Hugtand2 <hugz@outlook.dk>
2026-09-18 21:48:32 +0200

    Beat the level
    Der kommer nu en menu op når du vinder et level!
    
    Du får 1-3 stjerner
    
    1 stjerne for at klare det (garanteret)
    
    2 stjerner for at nå en grænse du kan ændre som export variabel i inspector på ExitChecker for hvert individuelle level
    
    3 stjerner for at nå en grænse du kan ændre som export variabel i inspector på ExitChecker for hvert individuelle level (den skal være sværere/lavere tal end sidste.)
    
    Jeg har talt for de første 5 levels, at man kan klare dem på:
    LevelOne:11
    LevelTwo:52
    LevelThree:20
    LevelFour:93
    LevelFive:28
    
    Det er sådan set det.
    
    Det kan *være*, at du skal sætte dit spil til "stretch to fit" under embedded window sizing for at det virker rigtigt. IDK, måske.

--- Catacomb-Catastrophe/Scripts/exit_checker.gd
+++ Catacomb-Catastrophe/Scripts/exit_checker.gd
@@ -2,22 +2,82 @@ extends Node2D
 
 @onready var ExitRaycast: RayCast2D = $ExitRaycast
 @onready var Sarc: StaticBody2D = $"../Sarcophagus"
+# Export specific info for this level only
+
 @export var next_level_path: String
+@export var second_goldstar_req: int
+@export var third_goldstar_req: int
+
 @onready var next: Button = $"../UI/NextLevelButton"
 @onready var retry: Button = $"../UI/RetryButton"
 
-# Called when the node enters the scene tree for the first time.
+# Get spiller for at tælle moves
+@onready var player: CharacterBody2D = $"../CharacterBody2D"
+# Spiller move count (inkluderer også push_block)
+@onready var move_count: int = 0
+
+
+# Ny level beat grafik
+@onready var victory_background: Sprite2D = $"../UI/VictoryBg2"
+@onready var pyramid: Sprite2D = $"../UI/Pyramide"
+
+@onready var graystar1: Sprite2D = $"../UI/GrayStar1"
+@onready var graystar2: Sprite2D = $"../UI/GrayStar2"
+@onready var graystar3: Sprite2D = $"../UI/GrayStar3"
+
+@onready var goldstar1: Sprite2D = $"../UI/GoldStar1"
+@onready var goldstar2: Sprite2D = $"../UI/GoldStar2"
+@onready var goldstar3: Sprite2D = $"../UI/GoldStar3"
+
+# Labels for at fortælle hvor mange moves man må bruge
+@onready var goldstarlabel2: Label = $"../UI/GoldStarLabel2"
+@onready var goldstarlabel3: Label = $"../UI/GoldStarLabel3"
+@onready var starlevelbeat: Label = $"../UI/StarLevelBeat"
+
+
 func _ready() -> void:
 	Sarc.block_moved_exit_check.connect(exit_checks)
 	next.pressed.connect(on_next_level_button_pressed)
 	retry.pressed.connect(on_retry_button_pressed)
+	player.player_moved.connect(count_player_move)
 
 func exit_checks() -> void:
 	print("Exit checked")
 	if ExitRaycast.is_colliding():
 		print("Level Won! Bring up UI now")
+		MusicPlayer.stop_music()
+		MusicPlayer.play_custom_track("res://Music/egyptian swag.mp3")
+		# Show det hele
 		next.show()
 		retry.show()
+		victory_background.show()
+		pyramid.show()
+		# Altid show grå stjerner
+		graystar1.show()
+		graystar2.show()
+		graystar3.show()
+		# Første guld for at vinde banen
+		# Man behøver måske ikke graystar1, men fuck det
+		goldstar1.show()
+		# Tjek om de har fået goldstar 2 og 3. Det er baseret på steps taken
+		if move_count <= second_goldstar_req:
+			print("Second gold star reached")
+			goldstar2.show()
+		if move_count <= third_goldstar_req:
+			print("Third gold star reached")
+			goldstar3.show()
+		# Vis labels for move requirement ift. stjerner
+		goldstarlabel2.text = ("Under " + str(second_goldstar_req+1) + " moves")
+		goldstarlabel3.text = ("Under " + str(third_goldstar_req+1) + " moves")
+		starlevelbeat.show()
+		goldstarlabel2.show()
+		goldstarlabel3.show()
+		
+		
+		
+func count_player_move() -> void:
+	move_count += 1
+	print("Move count: " + str(move_count))
 
 
 func on_retry_button_pressed() -> void:
