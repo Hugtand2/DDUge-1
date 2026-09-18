699edd230b00df20eced3960a4ea2eb8ce00d699
Widal <mikkel.widal@gmail.com>
2026-09-18 14:43:25 +0200

    Level 1-8 done

--- Catacomb-Catastrophe/Scripts/character_body_2d.gd
+++ Catacomb-Catastrophe/Scripts/character_body_2d.gd
@@ -5,6 +5,7 @@ var sprite_node_pos_tween: Tween
 
 func _ready() -> void:
 	$HurtBox.body_entered.connect(_on_body_entered)
+	$Sprite2D.frame = 0
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer and body.name == "SpikesTileMapLayer":
