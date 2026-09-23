54c83c06fc46cb51dbffc13477c02288fa9201c4
Widal <mikkel.widal@gmail.com>
2026-09-23 14:06:51 +0200

    Made cutscene skip

--- Catacomb-Catastrophe/Cutscenes/Intro_cutscene/intro_cutscene.gd
+++ Catacomb-Catastrophe/Cutscenes/Intro_cutscene/intro_cutscene.gd
@@ -8,10 +8,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		SceneManager.change_scene("res://Scenes/LevelOne.tscn")
 	pass
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	SceneManager.change_scene("res://Scenes/LevelOne.tscn")
 	pass # Replace with function body.
--- Catacomb-Catastrophe/Scripts/level_one.gd
+++ Catacomb-Catastrophe/Scripts/level_one.gd
@@ -4,6 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Levels.setup()
+	$UI/RetryLabel.show()
+	$UI/ArrowkeyLabel.show()
+	$UI/SkipLabel.hide()
 	#$Camera2D/LevelViewportUI/RetryLabel.show()
 	#$Camera2D/LevelViewportUI/ArrowkeyLabel.show()
 
